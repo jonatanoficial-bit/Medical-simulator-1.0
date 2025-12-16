6df225d913ffe03a778b242b0c48b3217fefb8a3
jonatanoficial-bit <jonatanoficial@gmail.com>
2025-12-16 08:05:41 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
+const CACHE_NAME = "eds-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./cases.json",
+  "./exams.json",
+  "./manifest.json"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
